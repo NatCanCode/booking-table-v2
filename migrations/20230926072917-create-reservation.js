df7bd6c2874e998b7c97807f00fd875b8c1cb0c9
NatCanCode <nathalie.piat1@gmail.com>
2023-09-26 09:30:02 +0200

    Create room, user and reservation models

---
+++ migrations/20230926072917-create-reservation.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reservations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      note: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER
+      },
+      roomId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reservations');
+  }
+};
---
+++ models/reservation.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Reservation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Reservation.init({
+    date: DataTypes.DATE,
+    name: DataTypes.STRING,
+    note: DataTypes.STRING,
+    status: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    spotId: DataTypes.INTEGER,
+    roomId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Reservation',
+  });
+  return Reservation;
+};
