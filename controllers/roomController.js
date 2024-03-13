97e23b6b6d782a9d9470074218682e98bc99125a
NatCanCode <nathalie.piat1@gmail.com>
2024-03-13 16:10:32 +0100

    Create roomController.js

---
+++ controllers/roomController.js
@@ -0,0 +1,87 @@
+// roomController.js
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../config/config.json')['development'];
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect
+});
+const Room = require('../models/room.js')(sequelize, DataTypes);
+
+const isAdmin = require("../routes/isAdmin.js");
+
+// GET all rooms
+async function getAllRooms(req, res, next) {
+    try {
+        const rooms = await Room.findAll();
+        res.json({ rooms });
+    } catch (error) {
+        next(error);
+    }
+}
+
+// POST create room
+async function createRoom(req, res, next) {
+    try {
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(400).json({ error: "Room name is required." });
+        }
+
+        const room = await Room.create({ name });
+        res.json({ room });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while creating the room." });
+        next(error);
+    }
+}
+
+// PUT update room
+async function updateRoom(req, res, next) {
+    const roomId = req.params.roomId;
+    const { name } = req.body;
+
+    try {
+        const room = await Room.findByPk(roomId);
+
+        if (!room) {
+            return res.status(404).json({ error: "Room not found." });
+        }
+
+        room.name = name;
+        await room.save();
+        res.json({ room });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while updating the room." });
+        next(error);
+    }
+}
+
+// DELETE room
+async function deleteRoom(req, res, next) {
+    const roomId = req.params.roomId;
+
+    try {
+        const room = await Room.findByPk(roomId);
+
+        if (!room) {
+            return res.status(404).json({ error: "Room not found." });
+        }
+
+        await room.destroy();
+        res.json({ message: "Room deleted." });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while deleting the room." });
+        next(error);
+    }
+}
+
+module.exports = {
+    getAllRooms,
+    createRoom,
+    updateRoom,
+    deleteRoom
+};
--- routes/room.js
+++ routes/room.js
@@ -1,66 +1,86 @@
 const express = require('express');
 const router = express.Router();
-const { Sequelize, DataTypes } = require('sequelize');
-const config = require('../config/config.json')['development'];
+const roomController = require('../controllers/roomController');
 const { isAdmin } = require("./isAdmin.js");
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect
-});
-const Room = require('../models/room')(
-    sequelize, DataTypes
-);
+// GET all rooms
+router.get('/', isAdmin, roomController.getAllRooms);
 
-/* GET Room */
-router.get("/", isAdmin, async (req, res, next) => {
-    const rooms = await Room.findAll();
-    res.json({ rooms });
-});
+// POST Create Room
+router.post("/", roomController.createRoom);
 
-/* Post Room */
-router.post("/", async (req, res, next) => {
-    const { name } = req.body;
+// PUT Update Room
+router.put("/:roomId", roomController.updateRoom);
 
-    if (!name) {
-        return res.status(400).json({ error: "Room name is required." });
-    }
-
-    const room = await Room.create({ name });
-    res.json({ room });
-});
-
-/* Put Room. */
-router.put("/:roomId", async (req, res, next) => {
-    const { roomId } = req.params;
-    const { name } = req.body;
-
-    if (!name) {
-        return res.status(400).json({ error: "Room name is required." });
-    }
-
-    const room = await Room.findByPk(roomId);
-
-    if (!room) {
-        return res.status(404).json({ error: "Room not found." });
-    }
-
-    room.name = name;
-    await room.save();
-    res.json({ room });
-});
-
-/* Delete Room */
-router.delete("/:roomId", async (req, res, next) => {
-    const { roomId } = req.params;
-    const room = await Room.findByPk(roomId);
-
-    if (!room) {
-        return res.status(404).json({ error: "Room not found." });
-    }
-
-    await room.destroy();
-    res.json({ message: "Room deleted." });
-});
+// DELETE Room
+router.delete("/:roomId", roomController.deleteRoom);
 
 module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+// const { Sequelize, DataTypes } = require('sequelize');
+// const config = require('../config/config.json')['development'];
+// const { isAdmin } = require("./isAdmin.js");
+
+// const sequelize = new Sequelize(config.database, config.username, config.password, {
+//     host: config.host,
+//     dialect: config.dialect
+// });
+// const Room = require('../models/room')(
+//     sequelize, DataTypes
+// );
+
+// /* GET Room */
+// router.get("/", isAdmin, async (req, res, next) => {
+//     const rooms = await Room.findAll();
+//     res.json({ rooms });
+// });
+
+// /* Post Room */
+// router.post("/", async (req, res, next) => {
+//     const { name } = req.body;
+
+//     if (!name) {
+//         return res.status(400).json({ error: "Room name is required." });
+//     }
+
+//     const room = await Room.create({ name });
+//     res.json({ room });
+// });
+
+// /* Put Room. */
+// router.put("/:roomId", async (req, res, next) => {
+//     const { roomId } = req.params;
+//     const { name } = req.body;
+
+//     if (!name) {
+//         return res.status(400).json({ error: "Room name is required." });
+//     }
+
+//     const room = await Room.findByPk(roomId);
+
+//     if (!room) {
+//         return res.status(404).json({ error: "Room not found." });
+//     }
+
+//     room.name = name;
+//     await room.save();
+//     res.json({ room });
+// });
+
+// /* Delete Room */
+// router.delete("/:roomId", async (req, res, next) => {
+//     const { roomId } = req.params;
+//     const room = await Room.findByPk(roomId);
+
+//     if (!room) {
+//         return res.status(404).json({ error: "Room not found." });
+//     }
+
+//     await room.destroy();
+//     res.json({ message: "Room deleted." });
+// });
+
+// module.exports = router;
